ea095cf3890b1a34e904a69ead46bb8a7138561b
Chris Bingham <binghamchris@users.noreply.github.com>
2024-05-01 19:40:40 +0200

    Working with compression
    Added function to compress the JSON file, to optimise S3 costs.
    General code clean up.

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const fs = require('fs');
 const contentfulExport = require('contentful-export');
+const AdmZip = require("adm-zip");
 
 exports.handler = async (event) => {
-  var datetime = new Date();
-  var isodate = datetime.toISOString().replaceAll('-', '/').replaceAll(':', '-').replace('T', '/');
-  var s3path = isodate.slice(0, 10);
-  var filename = isodate.slice(11) + '.json';
-
-  const local_backup_path = '/tmp';
-  const file_name =  filename;
+  const datetime = new Date();
+  const isodate = datetime.toISOString().replaceAll('-', '/').replaceAll(':', '-').replace('T', '/');
+  const s3Path = isodate.slice(0, 10);
+  const filenameBase = isodate.slice(11);
+  const localBackupPath = '/tmp';
+  const contentfulExportFilename = `${filenameBase}.json`;
+  const contentfulExportFilePath = `${localBackupPath}/${contentfulExportFilename}`;
+  const zipFilename = `${filenameBase}.zip`;
+  const zipFilePath = `${localBackupPath}/${zipFilename}`;
   const options = {
     spaceId: process.env.SPACE_ID,
     environmentId: process.env.SPACE_ENV,
     managementToken: process.env.MANAGEMENT_TOKEN,
     deliveryToken: process.env.DELIVERY_TOKEN,
-    contentFile: file_name,
-    exportDir: local_backup_path,
+    contentFile: contentfulExportFilename,
+    exportDir: localBackupPath,
     useVerboseRenderer: false,
     saveFile: true,
     includeDrafts: true,
@@ -26,19 +29,20 @@ exports.handler = async (event) => {
 
   try {
     const result = await contentfulExport(options);
-    console.log('Data downloaded successfully from Contentful for Space ID: ' + process.env.SPACE_ID + ' and Envionment: ' + process.env.SPACE_ENV);
+    console.log(`Data downloaded successfully from Contentful for Space ID: ${process.env.SPACE_ID} and Environment: ${process.env.SPACE_ENV}`);
 
-    // Save file to AWS S3
+    console.log('Compressing backup');
+    createZipArchive(contentfulExportFilePath, zipFilePath)
+
     console.log('Preparing file for AWS S3');
-    const outputFile = local_backup_path + "/" + file_name;
-    let fileBuffer = new Buffer(fs.readFileSync(outputFile));
-    fs.unlinkSync(outputFile);
+    let fileBuffer = new Buffer.from(fs.readFileSync(zipFilePath));
+    fs.unlinkSync(zipFilePath);
     
-    await uploadFile(fileBuffer, s3path + '/' + filename);
+    await uploadFile(fileBuffer, `${s3Path}/${zipFilename}`);
     
-    return sendResponse(200, outputFile);
+    return sendResponse(200, zipFilePath);
   } catch (err) {
-    console.log('ERROR: ', err);
+    console.log(`ERROR: ${err}`);
     return sendResponse(500, err);
   };
 };
@@ -61,3 +65,16 @@ const uploadFile = async (buffer, key) => {
 
   return await s3.putObject(params).promise();
 };
+
+const createZipArchive = (inputFile, outputFile) => {
+  try {
+    console.log(`Compressing: ${inputFile}`)
+    const zip = new AdmZip();
+    zip.addLocalFile(inputFile);
+    console.log(`Output: ${outputFile}`)
+    zip.writeZip(outputFile);
+    console.log(`Created ${outputFile} successfully`);
+  } catch (e) {
+    console.log(`Something went wrong. ${e}`);
+  }
+}
